69dddf7f4d8a72cd0497d4632e8ca74d7f51c1e2
Alfred Schilken <alfred@schilken.de>
2021-12-07 15:20:29 +0100

    add tester.printToConsole in widget_test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,14 @@ void main() {
   testWidgets('Endless counter with defaultTimeout',
       (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
+    tester.printToConsole('before pumpAndSettle ${tester.testDescription}');
     await tester.pumpAndSettle();
   });
 
   testWidgets('Endless counter with 10 minutes timeout',
       (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
-
+    tester.printToConsole('before pumpAndSettle ${tester.testDescription}');
     await tester.pumpAndSettle(const Duration(milliseconds: 100),
         EnginePhase.sendSemanticsUpdate, const Duration(minutes: 10));
   });
@@ -19,7 +20,7 @@ void main() {
   testWidgets('Endless counter with 1 minute timeout and duration 100ms',
       (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
-
+    tester.printToConsole('before pumpAndSettle ${tester.testDescription}');
     await tester.pumpAndSettle(const Duration(milliseconds: 100),
         EnginePhase.sendSemanticsUpdate, const Duration(minutes: 1));
   });
@@ -27,7 +28,7 @@ void main() {
   testWidgets('Endless counter with 1 minute timeout and duration 200ms',
       (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
-
+    tester.printToConsole('before pumpAndSettle ${tester.testDescription}');
     await tester.pumpAndSettle(const Duration(milliseconds: 200),
         EnginePhase.sendSemanticsUpdate, const Duration(minutes: 1));
   });
