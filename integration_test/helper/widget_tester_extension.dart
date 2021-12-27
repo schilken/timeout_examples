6c1c6e9cc2c149a863b52d9db73f4d6c479a8bfc
Alfred Schilken <alfred@schilken.de>
2021-12-27 15:23:22 +0100

    fix typo

--- integration_test/helper/widget_tester_extension.dart
+++ integration_test/helper/widget_tester_extension.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-extension PumpAndSettleWithTtimeout on WidgetTester {
+extension PumpAndSettleWithTimeout on WidgetTester {
   Future<int> pumpAndSettleWithTimeout({int seconds = 30}) async {
     return pumpAndSettle(const Duration(milliseconds: 100),
         EnginePhase.sendSemanticsUpdate, Duration(seconds: seconds));
